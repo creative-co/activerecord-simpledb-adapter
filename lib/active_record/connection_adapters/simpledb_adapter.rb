6023f87b307c31d0308680d27c43689234ba3d64
gorkunov <alexander.gorkunov@gmail.com>
2011-04-20 17:37:09 +0400

    fixed compatibility with rails 3.0.7

--- lib/active_record/connection_adapters/simpledb_adapter.rb
+++ lib/active_record/connection_adapters/simpledb_adapter.rb
@@ -5,6 +5,7 @@ require 'uuidtools'
 
 require 'active_record/connection_adapters/simpledb_adapter/misc/aws_overrides'
 require 'active_record/connection_adapters/simpledb_adapter/misc/simpledb_logger'
+require 'active_record/connection_adapters/simpledb_adapter/finder_methods'
 require 'active_record/connection_adapters/simpledb_adapter/base'
 require 'active_record/connection_adapters/simpledb_adapter/validations'
 require 'active_record/connection_adapters/simpledb_adapter/table_definition'
