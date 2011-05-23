9e03d075e9517819f85d8538b08b0cc630b8255c
gorkunov <alexander.gorkunov@gmail.com>
2011-05-23 17:30:48 +0400

    updated gems

--- lib/active_record/connection_adapters/simpledb_adapter/base.rb
+++ lib/active_record/connection_adapters/simpledb_adapter/base.rb
@@ -1,14 +1,12 @@
 require 'aws'
 module ActiveRecord
   class Base
-    def self.included(base)
-      puts "test"
-    end
     def self.simpledb_connection(config) # :nodoc:
 
       config = config.symbolize_keys
       #fix this (replace to module)
-      alias_method_chain :initialize, :defaults
+	  unless method_defined? :initialize_without_defaults
+      alias_method_chain(:initialize, :defaults) \
 
       ConnectionAdapters::SimpleDBAdapter.new nil, logger,
           config[:access_key_id],
@@ -37,29 +35,29 @@ module ActiveRecord
     end
 
 
-    def initialize_with_defaults(attrs = nil)
-      initialize_without_defaults(attrs) do
-        safe_attribute_names = []
-        if attrs
-          stringified_attrs = attrs.stringify_keys
-          safe_attrs =  sanitize_for_mass_assignment(stringified_attrs)
-          safe_attribute_names = safe_attrs.keys.map { |x| x.to_s }
-        end
+	def initialize_with_defaults(attrs = nil)
+	  initialize_without_defaults(attrs) do
+		safe_attribute_names = []
+		if attrs
+		  stringified_attrs = attrs.stringify_keys
+		  safe_attrs =  sanitize_for_mass_assignment(stringified_attrs)
+		  safe_attribute_names = safe_attrs.keys.map { |x| x.to_s }
+		end
 
-        ActiveRecord::Base.connection.columns_definition(self.class.table_name).columns_with_defaults.each do |column|
-          if !safe_attribute_names.any? { |attr_name| attr_name =~ /^#{column.name}($|\()/ }
-            value =  if column.default.is_a? Proc
-                       column.default.call(self)
-                     else
-                       column.default
-                     end
-            __send__("#{column.name}=", value)
-            changed_attributes.delete(column.name)
-          end
-        end
-        yield(self) if block_given?
-      end
-    end
+		ActiveRecord::Base.connection.columns_definition(self.class.table_name).columns_with_defaults.each do |column|
+		  if !safe_attribute_names.any? { |attr_name| attr_name =~ /^#{column.name}($|\()/ }
+			value =  if column.default.is_a? Proc
+					   column.default.call(self)
+					 else
+					   column.default
+					 end
+			__send__("#{column.name}=", value)
+			changed_attributes.delete(column.name)
+		  end
+		end
+		yield(self) if block_given?
+	  end
+	end
 
     def self.batch &block
       connection.begin_batch
